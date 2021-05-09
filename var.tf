e4b8c53e31c7fc07e2f1d5802dcd8e99143ef026
Lakshith DV <lakshithdv@MacBook-Pro.local>
2021-05-09 10:40:20 +0530

    terraform script update

--- var.tf
+++ var.tf
@@ -6,9 +6,11 @@ variable "AWS_REGION" {
 variable "AWS_SECRAT_KRY" {
     type = string
     description = "Enter the aws secrate key"
+    default = "0gX170vtVlVV9HhRo0jJtSvUQx8urKiJP5uKMWxH"
 }
 
 variable "AWS_ACCESS_KEY" {
     type = string
     description = "Enter the aws access key"
+    default = "AKIAXILQYCM6VJPQIASM"
 }
